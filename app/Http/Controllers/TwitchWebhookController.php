d24b118193af7e1889427921588185ed26ca2482
Michael Phillips <michaeljoelphillips@gmail.com>
2019-11-07 01:18:11 -0600

    Notify the channel with the streamer's name.

--- app/Http/Controllers/TwitchWebhookController.php
+++ app/Http/Controllers/TwitchWebhookController.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
+use BotMan\Drivers\Slack\SlackDriver;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use BotMan\Drivers\Slack\SlackDriver;
 
 class TwitchWebhookController
 {
@@ -20,6 +20,26 @@ class TwitchWebhookController
 
     public function __invoke(Request $request): Response
     {
+        if ($this->userIsLive($request)) {
+            $user = $this->getUsernameFromRequest($request);
+
+            $this->botman->say(sprintf('%s is streaming on Twitch!', $user), '#thegang', SlackDriver::class);
+        }
+
         return new Response('', 200);
+    }
+
+    private function userIsLive(Request $request): bool
+    {
+        $request = json_decode($request->getContent());
+
+        return false === empty($request->data);
+    }
+
+    private function getUsernameFromRequest(Request $request): string
+    {
+        $request = json_decode($request->getContent());
+
+        return array_pop($request->data)->user_name;
     }
 }
